e16f11485cb26772351ed50baea94de14ad00e42
Gongjunzhe12210401 <2428950578@qq.com>
2023-12-14 20:04:18 +0800

    a

--- Learning.v
+++ Learning.v
@@ -1,72 +1,96 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/09 16:24:31
+// Design Name: 
+// Module Name: mode_learn
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module mode_learn(
     input wire clk, // Clock signal
     input wire [1:0] song_select, // choose song
-    input wire [6:0] switches, // 7¸ö¿ª¹ØÊäÈë
-    output reg [3:0] note_to_play, // out_put to buzzer
+    input wire [6:0] switches, // 7ä¸ªå¼€å…³è¾“å…¥
+    input wire rst, // Reset signal
+    output reg [3:0] note_to_play, // output to buzzer
     output reg [6:0] led_out
 );
 
-//parameter second = 70000000, song_time=61,music0=4'b0000,
-// music1=4'b0001, music2=4'b0010,music3=4'b0011,music4=4'b0100,music5=4'b0101,music6=4'b0110,music7=4'b0111,
-// led1=7'b0000001,led2=7'b0000010,led3=7'b0000100,led4=7'b0001000,led5=7'b0010000,
-// led6=7'b0100000,led7=7'b1000000,led8=7'b0000000;
-//// only seven now,but not enough
+parameter second = 70000000, song_time=61,music0=4'b0000,
+ music1=4'b0001, music2=4'b0010, music3=4'b0011, music4=4'b0100, music5=4'b0101, music6=4'b0110, music7=4'b0111,
+ led1=7'b0000001, led2=7'b0000010, led3=7'b0000100, led4=7'b0001000, led5=7'b0010000,
+ led6=7'b0100000, led7=7'b1000000, led8=7'b0000000;
+// only seven now, but not enough
 
+wire [3:0] song[song_time-1:0];
+wire [song_time*4-1:0] song_packed;
 
-// wire [3:0] song[song_time-1:0];
-//wire [song_time*4-1:0] song_packed;
+Lib lib_inst(
+    .clk(clk),
+    .song_select(song_select),
+    .song_packed(song_packed)
+);
 
+// Unpack song
+genvar i;
+generate
+    for (i = 0; i < song_time; i = i + 1) begin : unpack_loop
+        assign song[i] = song_packed[(4*i)+3 : 4*i];
+    end
+endgenerate
 
-//Lib lib_inst(
-//    .clk(clk),
-//    .song_select(song_select),
-//    .song_packed(song_packed)
-//);
+// Play song
+integer play_position = 0;
+reg note_played; // æ ‡è®°æ˜¯å¦å·²å¯¹å½“å‰éŸ³ç¬¦ä½œå‡ºååº”
 
+initial begin
+    note_played = 1'b1; // åˆå§‹è®¾ç½®ä¸º1ï¼Œä»¥ä¾¿ä»Žç¬¬ä¸€ä¸ªéŸ³ç¬¦å¼€å§‹
+end
 
-////unpack song
-//genvar i;
-//generate
-//    for (i = 0; i < song_time; i = i + 1) begin : unpack_loop
-//        assign song[i] = song_packed[(4*i)+3 : 4*i];
-//    end
-//endgenerate
+always @(posedge clk) begin
+    if (rst) begin
+            play_position <= 0;
+            note_played <= 1'b1;
+            note_to_play <= 4'b0000;
+            led_out <= 7'b0000000;
+        end
+    // æ£€æŸ¥æ˜¯å¦éœ€è¦ç§»åŠ¨åˆ°ä¸‹ä¸€ä¸ªéŸ³ç¬¦
+    else if (note_played) begin
+        play_position <= (play_position < song_time - 1) ? play_position + 1 : 0;
+        note_played <= 1'b0; // é‡ç½®æ ‡å¿—
+    end
 
-////play song
-//integer play_position = 0;
-//reg note_played; // ±ê¼ÇÊÇ·ñÒÑ¶Ôµ±Ç°Òô·û×÷³ö·´Ó¦
+    // è®¾ç½®LEDè¾“å‡ºå’ŒéŸ³ç¬¦
+    case (song[play_position])
+        music1: led_out <= led1;
+        music2: led_out <= led2;
+        music3: led_out <= led3;
+        music4: led_out <= led4;
+        music5: led_out <= led5;
+        music6: led_out <= led6;
+        music7: led_out <= led7;
+        default: led_out <= led8; // å¦‚æžœæ²¡æœ‰åŒ¹é…çš„éŸ³ç¬¦
+    endcase
 
-//initial begin
-//    note_played = 1'b1; // ³õÊ¼ÉèÖÃÎª1£¬ÒÔ±ã´ÓµÚÒ»¸öÒô·û¿ªÊ¼
-//end
+    note_to_play <= song[play_position];
 
-//always @(posedge clk) begin
-//    // ¼ì²éÊÇ·ñÐèÒªÒÆ¶¯µ½ÏÂÒ»¸öÒô·û
-//    if (note_played && (play_position < song_time-1)) begin
-//        play_position <= play_position + 1;
-//        note_played <= 1'b0; // ÖØÖÃ±êÖ¾
-//    end else if (note_played) begin
-//        play_position <= 0; // ´ÓÍ·¿ªÊ¼
-//        note_played <= 1'b0; // ÖØÖÃ±êÖ¾
-//    end
-
-//    // ÉèÖÃLEDÊä³öºÍÒô·û
-//    case (song[play_position])
-//        music1: led_out <= led1;
-//        music2: led_out <= led2;
-//        music3: led_out <= led3;
-//        music4: led_out <= led4;
-//        music5: led_out <= led5;
-//        music6: led_out <= led6;
-//        music7: led_out <= led7;
-//        default: led_out <= led8; // Èç¹ûÃ»ÓÐÆ¥ÅäµÄÒô·û
-//    endcase
-//    note_to_play <= song[play_position];
-
-//    // ¼ì²éÊÇ·ñ´ò¿ªÁËÓëµ±Ç°Òô·ûÏàÓ¦µÄ¿ª¹Ø
-//    if (switches[note_to_play-1] && note_to_play != 0) begin
-//        note_played <= 1'b1; // µ±ÓÃ»§´ò¿ª¿ª¹ØÊ±£¬ÉèÖÃ±êÖ¾
-//    end
-//end
+    // æ£€æŸ¥æ˜¯å¦æ‰“å¼€äº†ä¸Žå½“å‰éŸ³ç¬¦ç›¸åº”çš„å¼€å…³
+    if (note_to_play == 0 || switches[note_to_play-1]) begin
+            note_played <= 1'b1;
+        end
+end
 
 endmodule
