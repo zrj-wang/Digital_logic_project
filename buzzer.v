f5b39d22805166130e1101eb98326b5fcae44854
zrj-wang <12210403@mail.sustech.edu.cn>
2023-12-18 00:27:09 +0800

    a

--- buzzer.v
+++ buzzer.v
@@ -1,78 +1,63 @@
-
 module Buzzer (
- input wire clk , // Clock signal
- input wire [3:0] note , // Note ( Input 1 outputs a signal for 'do , ' 2 for 're , ' 3 for 'mi , ' 4 ,and so on)
- output wire speaker , // Buzzer output signal
- input wire[2:0] mode, // Reset signal
- input wire[1:0] octave,//choose octave :01 lower octave; 10 higher ocative; elese maintain previous octave
- input wire[1:0] octave_auto
- ) ;
+    input wire clk, // Clock signal
+    input wire [3:0] note, // Note
+    output wire speaker, // Buzzer output signal
+    input wire [2:0] mode, // Mode signal
+    input wire [1:0] octave, // Octave selection: 01 for lower, 10 for higher, else standard
+    input wire [1:0] octave_auto
+);
 
- 
-integer muti;
+reg [31:0] frequency;
+wire [31:0] base_frequencies [7:0];
+reg [31:0] counter;
+reg pwm;
 
+// Base frequencies for notes
+assign base_frequencies[1] = 381680; // Frequency for 'do'
+assign base_frequencies[2] = 340136; // Frequency for 're'
+assign base_frequencies[3] = 303030; // Frequency for 'mi'
+assign base_frequencies[4] = 285714; // Frequency for 'fa'
+assign base_frequencies[5] = 255102; // Frequency for 'so'
+assign base_frequencies[6] = 227273; // Frequency for 'la'
+assign base_frequencies[7] = 202429; // Frequency for 'si'
 
- wire [31:0] notes [7:0];
- reg [31:0] counter ;
- reg pwm ;
- // Frequencies of do , re , mi , fa , so , la , si
- // Obtain the ratio of how long the buzzer should be active in one second
- assign notes [1]=381680;
- assign notes [2]=340136;
- assign notes [3]=303030;
- assign notes [4]=285714;
- assign notes [5]=255102;
- assign notes [6]=227273;
- assign notes [7]=202429;
-
-
- initial
- begin
- pwm =0;
- end
-
-
-
-// Set multiplier based on octave and mode
-always @(*) begin
-    case (mode)
-        3'b010: begin
-            case (octave_auto)
-                2'b01: muti = 2;   // Higher octave
-                2'b10: muti = 0;   // Lower octave
-                default: muti = 1; // Standard octave
-            endcase
-        end
-        3'b100: begin
-            case (octave)
-                2'b01: muti = 2;   // Higher octave
-                2'b10: muti = 0;   // Lower octave
-                default: muti = 1; // Standard octave
-            endcase
-        end
-        default: muti = 1; // Standard octave
-    endcase
+initial begin
+    pwm = 0;
+    counter = 0;
 end
 
+// Adjust frequency based on octave
+always @(*) begin
+    if (note > 0) begin
+        case (mode)
+            3'b010: // Auto mode
+                case (octave_auto)
+                    2'b01: frequency = base_frequencies[note] >> 1; // Lower octave
+                    2'b10: frequency = base_frequencies[note] << 1; // Higher octave
+                    default: frequency = base_frequencies[note];    // Standard octave
+                endcase
+            3'b100: // Manual mode
+                case (octave)
+                    2'b01: frequency = base_frequencies[note] >> 1; // Lower octave
+                    2'b10: frequency = base_frequencies[note] << 1; // Higher octave
+                    default: frequency = base_frequencies[note];    // Standard octave
+                endcase
+            default: frequency = base_frequencies[note];            // Standard octave
+        endcase
+    end else begin
+        frequency = 0;
+    end
+end
 
-// Control the buzzer output based on note and octave
+// Control the buzzer output based on the adjusted frequency
 always @(posedge clk) begin
-    if (counter < notes[note] || note == 0) begin
-        if (muti == 0) begin
-            // For lower octave, count half the speed
-            if (counter[0] == 1'b0) begin
-                counter <= counter + 1;
-            end
-        end else begin
-            // For standard and higher octave, count normal speed
-            counter <= counter + 1;
-        end
+    if (counter < frequency) begin
+        counter <= counter + 1;
     end else begin
         pwm <= ~pwm;
         counter <= 0;
     end
 end
 
-
- assign speaker = pwm ; // Output a PWM signal to the buzzer
- endmodule
+assign speaker = pwm; // Output a PWM signal to the buzzer
+endmodule
